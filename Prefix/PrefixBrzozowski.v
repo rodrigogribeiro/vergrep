a89635283762b601013e962adf755d21d9ba5666
Rodrigo <rodrigogribeiro@gmail.com>
2017-12-31 22:32:53 -0200

    Almost finished major code refactoring for extraction

--- Prefix/PrefixBrzozowski.v
+++ Prefix/PrefixBrzozowski.v
@@ -11,37 +11,46 @@ Fixpoint brzozowski_prefix (e : regex)(s : string) : bool :=
   match s with
   | EmptyString => null e
   | String a' s' =>
-    orb (null e) (brzozowski_prefix e s')
+    orb (null e) (brzozowski_prefix (deriv a' e) s')
   end.
 
 
-Definition brzozowski_prefix
-  : forall e s, {prefix e s} + {~ prefix e s}.
-  Hint Resolve empty_not_prefix cons_not_prefix_brzozowski.
-  refine (fix brzozowski_prefix e s : {prefix e s} + {~ prefix e s} :=
-            match s with
-            | EmptyString =>
-              match null e with
-              | left _ => left _
-              | right _ => right _
-              end
-            | String a s' =>
-              match null e with
-              | left _ => left _
-              | right _ => 
-                match brzozowski_prefix (deriv a e) s' with
-                | left _ => left _
-                | right _ => _
-                end
-              end
-            end) ; clear brzozowski_prefix ;
-    try solve [econstructor ; eauto ; reflexivity].
+Theorem brzozowski_prefix_sound
+  : forall s e, Is_true (brzozowski_prefix e s) -> prefix e s.
+Proof.
+  induction s ; intros e H ; simpl in *.
   +
-    intro H ; inverts* H.
-    apply empty_string_concat in H1.
-    destruct* H1. substs ; contradiction.
+    apply null_sound in H.
+    apply MkPrefix with (x := "")(y := "") ; auto.
   +
-    inverts* p.
-    eapply deriv_sound in H; eauto.
-    simpl in * ; econstructor ; eauto. reflexivity.
-Defined.
+    apply orb_prop_elim in H.
+    destruct H.
+    -
+      apply null_sound in H.
+      apply MkPrefix with (x := "")(y := String a s) ; auto.
+    -
+      apply IHs in H.
+      inverts* H.
+      apply deriv_sound in H0.
+      apply MkPrefix with (x := String a x)(y := y) ; auto.        
+Qed.
+
+Theorem brzozowski_prefix_complete
+  : forall s e, prefix e s -> Is_true (brzozowski_prefix e s).
+Proof.
+  induction s ; intros e H ; simpl in * ; inverts* H.
+  +
+    apply empty_string_concat in H1 ; destruct H1 ; substs.
+    apply null_complete in H0 ; auto.
+  +
+    destruct x ; simpl in *.
+    -
+      substs.
+      apply null_complete in H0.
+      apply orb_prop_intro.
+      left*.
+    -
+      injects H1.
+      apply deriv_complete in H0.
+      apply orb_prop_intro ; right*.
+Qed.
